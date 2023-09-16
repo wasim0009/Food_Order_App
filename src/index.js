50a9a0b289c4af85a6dbfcba3105ae224fcc9ef0
wasim0009 <mr.wasim006@gmail.com>
2023-09-16 20:21:32 +0530

    Context module is Completed

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import {BrowserRouter as Router} from "react-router-dom";
-ReactDOM.render(<Router><App/></Router>, document.getElementById("root"));
+import { BrowserRouter as Router } from "react-router-dom";
+import { StateProvider } from "./context/StateProvider";
+import { InitialState } from "./context/InitialState";
+import reducer from "./context/Reducer";
+ReactDOM.render(
+  <Router>
+    <StateProvider initialState={InitialState} reducer={reducer}>
+    <App />
+    </StateProvider>
+  </Router>,
+  document.getElementById("root")
+);
