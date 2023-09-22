6fa9dc3a5b85d427dbe0c0f6b7f442dd22a9fd33
wasim0009 <mr.wasim006@gmail.com>
2023-09-22 21:21:53 +0530

    Filtering Menu is Completed

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -3,70 +3,76 @@ import Delivery from "../components/img/delivery.png";
 import HeroBg from "../components/img/heroBg.png";
 import { heroData } from "../utils/data.js";
 
-const HomeComponent = () => {
+const HomeContainer = () => {
   return (
     <section
-      id="Home"
-      className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full h-screen"
+      className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full "
+      id="home"
     >
-      <div className="py-2 flex-1 flex flex-col items-start md:items-start justify-center gap-6">
-        <div className="flex item-center gap-2 md:items-center justify-center bg-orange-200 px-4 py-1 rounded-full">
-          <p className="text-headingColor text: text-xl font-bold flex items-center text-base text-orange-600  font-semibold drop-shadow-xl">
+      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
+        <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
+          <p className="text-base text-orange-500 font-semibold">
             Bike Delivery
           </p>
-          <div className="w-10 h-10 rounded-full overflow-hidden drop-shadow-xl">
+          <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
             <img
               src={Delivery}
-              className="w-full h-full object-contain bg-white"
+              className="w-full h-full object-contain"
               alt="delivery"
             />
           </div>
         </div>
 
-        <p className="text-[2.5rem] md:text-[4.5rem] font-bold tracking-wide text-heading Color">
-          The Fastest Delivery in{" "}
-          <span className="text-orange-600 text-[3rem] md:text-[5rem]">
+        <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
+          The Fastest Delivery in
+          <span className="text-orange-600 text-[3rem] lg:text-[5rem]">
             Your City
           </span>
         </p>
-        <p className="text-base text-textColor md:text-left md:w-[80%] text-center ">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam,
-          assumenda? Enim cum aut veritatis modi officiis nihil cumque,
-          laudantium, iusto explicabo ducimus vel mollitia sint quibusdam
-          reiciendis consequuntur! Officia tempore molestiae inventore
-          perspiciatis!
+
+        <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima velit
+          eaque fugit distinctio est nam voluptatum architecto, porro iusto
+          deserunt recusandae ipsa minus eos sunt, dolores illo repellat facere
+          suscipit!
         </p>
-        <button className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg transition-all ease-in-out duration-100 hover:shadow-2xl">
+
+        <button
+          type="button"
+          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+        >
           Order Now
         </button>
       </div>
-      <div className="py-2 flex-1 items-center relative">
+      <div className="py-2 flex-1 flex items-center relative">
         <img
           src={HeroBg}
-          className="ml-auto h-420 w-full lg:w-auto lg:h-650 "
+          className=" ml-auto h-420 w-full lg:w-auto lg:h-650"
           alt="hero-bg"
         />
-        <div className="w-full h-full absolute top-0 left-0 flex items-center  justify-center lg:px-32 py-4 gap-4 flex-wrap drop-shadow-xl">
+
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32  py-4 gap-4 flex-wrap">
           {heroData &&
             heroData.map((n) => (
               <div
-                key={n.id} 
-                className="lg:w-190 p-4 bg-cardOverlay backdrop:blur-md flex flex-col items-center justify-center  rounded-3xl "
+                key={n.id}
+                className="  lg:w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
               >
                 <img
                   src={n.imgSrc}
-                  className=" w-20 lg:w-40 -mt-10 lg:-mt-20 "
-                  alt="i4"
+                  className="w-20 lg:w-40 -mt-10 lg:-mt-20 "
+                  alt="I1"
                 />
-                <p className="text-base lg:text-xl  font-bold  text-textColor mt-2 lg:mt-4">
+                <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
                   {n.name}
                 </p>
+
                 <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
                   {n.decp}
                 </p>
-                <p className="text-md font-semibold text-headingColor">
-                  <span className="text-xs text-red-600">$ </span>
-                  {n.price}
+
+                <p className="text-sm font-semibold text-headingColor">
+                  <span className="text-xs text-red-600">$</span> {n.price}
                 </p>
               </div>
             ))}
@@ -76,4 +82,4 @@ const HomeComponent = () => {
   );
 };
 
-export default HomeComponent;
+export default HomeContainer;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
-export {default as Header} from "./Header";
-export {default as MainConductor} from "./MainConductor";
-export {default as CreateContainer} from "./CreateContainer";
-export {default as HomeContainer} from "./HomeContainer";
-export {default as Loader} from "./Loader";
+export { default as Header } from "./Header";
+export { default as MainConductor } from "./MainConductor";
+export { default as CreateContainer } from "./CreateContainer";
+export { default as HomeContainer } from "./HomeContainer";
+export { default as Loader } from "./Loader";
+export { default as RowContainer } from "./RowContainer";
+export { default as MenuContainer } from "./MenuContainer";
